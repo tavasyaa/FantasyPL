94298a72ffbb1944e005931e2e81e7287c080ed5
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-04-08 18:02:06 -0500

    begun to collect player data

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import json, requests, os
 from pandas.io.json import json_normalize
 from matplotlib import pyplot as plt
 
-### SECTION 1: My team's performance
-
+# Fantasy team stats
 gwaverages = []
 gwnumber = []
 teamhistory = []
 teamrank = []
 
+# Player analysis
+pointspergame = []
+totalpoints = []
+totalminutes = []
+price = []
+
+
 # A function to get json data from the Fantasy PL API and write it to a file. 
-# We don't strictly need to write it to use it, but I'm doing that in case I want to use it for other purposes/look at it in Excel. 
+# We don't strictly need to write it to use it, but I'm doing that in case I want to use 
+# it for other purposes/look at it in Excel. 
 # data is type request. jsonResponse is type json. json.dump is a string of json.
 # Remember that 'elements' are players
 def get_json(file_path, type):
@@ -36,6 +43,14 @@ get_json(os.path.expanduser('~/Desktop/Code/FantasyPL/FantasyPLTeamHistory.json'
 with open(os.path.expanduser('~/Desktop/Code/FantasyPL/FantasyPLBootstrap.json')) as json_data:
 	bootstrapdata = json.load(json_data)
 
+	# Populating our arrays with data, the next thing we do is sort the data for PPG and PPM and see the top ones, then we can 
+	# use Points/$ as well later
+	for i in range(len(bootstrapdata['elements'])):
+		pointspergame.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['points_per_game']})
+		totalpoints.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['total_points']})
+		totalminutes.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['minutes']})
+		totalminutes.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['now_cost']})
+
 with open(os.path.expanduser('~/Desktop/Code/FantasyPL/FantasyPLTeamHistory.json')) as json_data:
 	historydata = json.load(json_data)
 
@@ -52,10 +67,13 @@ for i in range(len(historydata['current'])):
 	teamhistory.append(historydata['current'][i]['points'])
 	teamrank.append(historydata['current'][i]['overall_rank'])
 
-print('Gameweek averages:',gwaverages)
-print('For gameweek numbers:',gwnumber)
-print('My team scores:', teamhistory)
-print('My overall rankings:', teamrank)
+#print('Gameweek averages:',gwaverages)
+#print('For gameweek numbers:',gwnumber)
+#print('My team scores:', teamhistory)
+#print('My overall rankings:', teamrank)
+#print(list(bootstrapdata['elements'][0].keys()))
+print(len(pointspergame))
+
 
 # This plot shows general averages across gameweeks for players
 fig = plt.figure()
@@ -76,8 +94,3 @@ axis2.set_title('Fantasy PL: Rank History')
 axis2.legend()
 
 plt.show()
-
-
-
-
-
