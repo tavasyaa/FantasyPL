4c0c019cf22b5b9988602d27431284d8a23adfbe
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-04-09 00:12:26 -0500

    player data brought in, analyzed and tweakable, team setups to come

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ gwnumber = []
 teamhistory = []
 teamrank = []
 
-
-
-# A function to get json data from the Fantasy PL API and write it to a file. 
-# We don't strictly need to write it to use it, but I'm doing that in case I want to use 
-# it for other purposes/look at it in Excel. 
-# data is type request. jsonResponse is type json. json.dump is a string of json.
-# Remember that 'elements' are players
+# A function to get json data from the Fantasy PL API and write it to a file. We don't strictly need to write 
+#it to use it, but I'm doing that in case I want to use it for other purposes/look at it in Excel. 
+# data is type request. jsonResponse is type json. json.dump is a string of json. 'elements' are players
 def get_json(file_path, type):
 	# dump contents of bootstrap
 	if type == 'bootstrap':
@@ -79,29 +75,63 @@ axis2.set_ylabel('Overall rank #')
 axis2.set_title('Fantasy PL: Rank History')
 axis2.legend()
 
-plt.show()
+#plt.show()
 
 #SECTION 2: Player analysis
 
-pointspergame = []
-totalpoints = []
-totalminutes = []
-price = []
+playerdata = bootstrapdata['elements']
 
-# Populating our arrays with data, the next thing we do is sort the data for PPG and PPM and see the top ones, then we can 
-# use Points/$ as well later
-for i in range(len(bootstrapdata['elements'])):
-	pointspergame.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['points_per_game']})
-	totalpoints.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['total_points']})
-	totalminutes.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['minutes']})
-	price.append({bootstrapdata['elements'][i]['web_name']: bootstrapdata['elements'][i]['now_cost']})
+for i in range(len(playerdata)):
+	playerdata[i]['points_per_dollar'] = playerdata[i]['total_points'] / playerdata[i]['now_cost'] / 10
 
-print(pointspergame)
-print(totalpoints)
-print(totalminutes)
-print(price)
+	if playerdata[i]['minutes'] != 0:
+		playerdata[i]['points_per_minute'] = playerdata[i]['total_points'] / playerdata[i]['minutes']
+	else:
+		playerdata[i]['points_per_minute'] = 0
+
+# Sorts by a certain key in the dictionary
+sortedpoints = sorted(playerdata, key=lambda k: k['total_points'], reverse = True) 
+sortedminutes = sorted(playerdata, key=lambda k: k['minutes'], reverse = True) 
+sortedppg = sorted(playerdata, key=lambda k: k['points_per_game'], reverse = True)
+sortedppd = sorted(playerdata, key=lambda k: k['points_per_dollar'], reverse = True)
+sortedppm = sorted(playerdata, key=lambda k: k['points_per_minute'], reverse = True)
+
+print('TOP 20 PLAYERS ON THE FOLLOWING DIMENSIONS ARE:')
+
+# Points per minute are useful to identify bench players, or players who you know will play more in the future
+print('PPM: PPM: PPM: PPM: PPM: PPM: PPM: PPM: PPM: PPM: ')
+counter = 0
+for i in range(len(playerdata)):
+	if (sortedppm[i]['minutes']) > 500:
+		print(sortedppm[i]['web_name'], sortedppm[i]['points_per_minute'])
+		counter = counter + 1
+	if counter == 20:
+		break
+
+# To identify Mr. Reliables, they'll always play
+print('MINUTES: MINUTES: MINUTES: MINUTES: MINUTES: ')
+for i in range(20):
+	print(sortedminutes[i]['web_name'], sortedminutes[i]['minutes'])
+
+# Self-explanatory, it will be too expensive to feature a lot of them at once
+print('POINTS: POINTS: POINTS: POINTS: POINTS: POINTS: ')
+for i in range(20):
+	print(sortedpoints[i]['web_name'], sortedpoints[i]['total_points'])
+
+# Bang for your buck -- a key insight not provided by FPL
+print('PP$: PP$: PP$: PP$: PP$: PP$: PP$: PP$: PP$: ')
+for i in range(20):
+	print(sortedppd[i]['web_name'], sortedppd[i]['points_per_dollar'])
+
+# Points per game -- if they are returning from injury, a high PPG could mean they are a future great pick
+print('PPG: PPG: PPG: PPG: PPG: PPG: PPG: PPG: PPG: ')
+for i in range(20):
+	print(sortedppg[i]['web_name'], sortedppg[i]['points_per_game'])
 
 
+# SECTION 3: Building a team with the ranked data
+# then, you can go down the ppm or ppd or ppg array and see what team you can afford - and which gets the highest points tally
+# Restructure code? Set up excels etc in one file, do section 1 in one, and section 2 in the other possibly
 
 
 
